088012a9b233c77e46c2301af07233def5f1ac38
83286415 <83286415@163.com>
2019-06-18 17:34:23 +0800

    5.4-visualizing-what-convnets-learn.py added 5.3.2 py modified model2, model3 saved upload

--- 5.3.2-using-a-pretrained-convnet-with-fine-tuning.py
+++ 5.3.2-using-a-pretrained-convnet-with-fine-tuning.py
@@ -1,2 +1,195 @@
 import keras
 print(keras.__version__)  # 2.2.4
+
+from keras.applications import VGG16
+
+# pre-trained network
+conv_base = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(150, 150, 3))
+
+print(conv_base.summary())
+'''
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #   
+=================================================================
+input_1 (InputLayer)         (None, 150, 150, 3)       0         
+_________________________________________________________________
+block1_conv1 (Conv2D)        (None, 150, 150, 64)      1792        # the bottom layer
+_________________________________________________________________
+block1_conv2 (Conv2D)        (None, 150, 150, 64)      36928     
+_________________________________________________________________
+block1_pool (MaxPooling2D)   (None, 75, 75, 64)        0         
+_________________________________________________________________
+block2_conv1 (Conv2D)        (None, 75, 75, 128)       73856     
+_________________________________________________________________
+block2_conv2 (Conv2D)        (None, 75, 75, 128)       147584    
+_________________________________________________________________
+block2_pool (MaxPooling2D)   (None, 37, 37, 128)       0         
+_________________________________________________________________
+block3_conv1 (Conv2D)        (None, 37, 37, 256)       295168    
+_________________________________________________________________
+block3_conv2 (Conv2D)        (None, 37, 37, 256)       590080    
+_________________________________________________________________
+block3_conv3 (Conv2D)        (None, 37, 37, 256)       590080    
+_________________________________________________________________
+block3_pool (MaxPooling2D)   (None, 18, 18, 256)       0         
+_________________________________________________________________
+block4_conv1 (Conv2D)        (None, 18, 18, 512)       1180160   
+_________________________________________________________________
+block4_conv2 (Conv2D)        (None, 18, 18, 512)       2359808   
+_________________________________________________________________
+block4_conv3 (Conv2D)        (None, 18, 18, 512)       2359808   
+_________________________________________________________________
+block4_pool (MaxPooling2D)   (None, 9, 9, 512)         0         
+_________________________________________________________________
+block5_conv1 (Conv2D)        (None, 9, 9, 512)         2359808     # the top layer
+_________________________________________________________________
+block5_conv2 (Conv2D)        (None, 9, 9, 512)         2359808   
+_________________________________________________________________
+block5_conv3 (Conv2D)        (None, 9, 9, 512)         2359808   
+_________________________________________________________________
+block5_pool (MaxPooling2D)   (None, 4, 4, 512)         0         
+=================================================================
+Total params: 14,714,688
+Trainable params: 14,714,688
+Non-trainable params: 0
+_________________________________________________________________
+'''
+
+# unfreeze the top layer
+conv_base.trainable = True  # unfreeze all layers here
+
+set_trainable = False
+for layer in conv_base.layers:  # loop in all layers
+    if layer.name == 'block5_conv1':  # the top layer
+        set_trainable = True          # unfreeze the top layer for fine tuning
+    if set_trainable:
+        layer.trainable = True
+    else:
+        layer.trainable = False       # freeze the other layers
+
+
+from keras import models
+from keras import layers
+from keras import optimizers
+
+model = models.Sequential()
+model.add(conv_base)  # add pre-trained network model into this model
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+
+import os
+from keras.preprocessing.image import ImageDataGenerator
+
+base_dir = 'D:/AI/deep-learning-with-python-notebooks-master/cats_and_dogs_small'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+# define train data generator (only) with data augmentation
+train_datagen = ImageDataGenerator(
+      rescale=1./255,  # All images will be rescaled by 1./255 (the max pixel value is 255. here its max is 1)
+      rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+# Note that the validation data should not be augmented!
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# config the train data generator after defining it above
+train_generator = train_datagen.flow_from_directory(
+        # This is the target directory
+        train_dir,
+        # All images will be resized to 150x150
+        target_size=(150, 150),
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+
+# compile the model to make conv_base modification work
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-5),
+              metrics=['acc'])
+
+# train the model. This process costs about 8 hours a time without GPU support.
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=100,
+      validation_data=validation_generator,
+      validation_steps=50)
+
+model.save('cats_and_dogs_small_4_fine_tuning.h5')
+
+
+# plot
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+
+# Exponential average shifting: new point value = previous point value * factor + point value * (1 - factor)
+# To make the curve smooth
+def smooth_curve(points, factor=0.8):
+  smoothed_points = []
+  for point in points:
+    if smoothed_points:
+      previous = smoothed_points[-1]
+      smoothed_points.append(previous * factor + point * (1 - factor))
+    else:
+      smoothed_points.append(point)
+  return smoothed_points
+
+
+plt.plot(epochs,
+         smooth_curve(acc), 'bo', label='Smoothed training acc')  # smooth_curve the acc dot line
+plt.plot(epochs,
+         smooth_curve(val_acc), 'b', label='Smoothed validation acc')  # smooth_curve the val_acc line
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs,
+         smooth_curve(loss), 'bo', label='Smoothed training loss')
+plt.plot(epochs,
+         smooth_curve(val_loss), 'b', label='Smoothed validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
+# evaluate the model
+test_generator = test_datagen.flow_from_directory(
+        test_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+test_loss, test_acc = model.evaluate_generator(test_generator, steps=50)
+print('test acc:', test_acc)
+print('test loss:', test_loss)
+
+print('5.3.2 done')
+print('---------------------------------------------------------------------------------------------------------------')
