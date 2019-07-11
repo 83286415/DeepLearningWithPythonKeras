add1e8c72c3c81abf7ca4bcfc9c7d36329f6aced
83286415 <83286415@163.com>
2019-07-11 17:28:20 +0800

    6.3.5 & 6.3.6 added

---
+++ 6.3.5-a-first-recurrent-baseline.py
@@ -0,0 +1,134 @@
+# FROM 6.3py
+# 6.3.1 prepare the climate data
+
+import keras
+print(keras.__version__)  # 2.1.6
+import os
+
+
+# prepare the climate data
+base_dir = 'D:/AI/deep-learning-with-python-notebooks-master'
+climate_dir = os.path.join(base_dir, 'jena_climate')
+fname = os.path.join(climate_dir, 'jena_climate_2009_2016.csv')
+
+f = open(fname)
+data = f.read()
+f.close()
+
+#  CSV file read
+lines = data.split('\n')
+header = lines[0].split(',')
+lines = lines[1:]  # title is not included
+
+# data analysis
+import numpy as np
+
+float_data = np.zeros((len(lines), len(header) - 1))  # len(header)-1: data time is not included
+for i, line in enumerate(lines):
+    values = [float(x) for x in line.split(',')[1:]]
+    float_data[i, :] = values  # put values of climate data into float_date list without data time
+
+
+def generator(data, lookback, delay, min_index, max_index,
+              shuffle=False, batch_size=128, step=6):
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lookback
+    while 1:
+        if shuffle:
+            rows = np.random.randint(
+                min_index + lookback, max_index, size=batch_size)
+        else:
+            if i + batch_size >= max_index:
+                i = min_index + lookback
+            rows = np.arange(i, min(i + batch_size, max_index))
+            i += len(rows)
+
+        samples = np.zeros((len(rows),
+                           lookback // step,
+                           data.shape[-1]))
+        targets = np.zeros((len(rows),))
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+        yield samples, targets
+
+
+lookback = 1440
+step = 6
+delay = 144
+batch_size = 128
+
+train_gen = generator(float_data,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=0,
+                      max_index=200000,
+                      shuffle=True,
+                      step=step,
+                      batch_size=batch_size)
+val_gen = generator(float_data,
+                    lookback=lookback,
+                    delay=delay,
+                    min_index=200001,
+                    max_index=300000,
+                    step=step,
+                    batch_size=batch_size)
+test_gen = generator(float_data,
+                     lookback=lookback,
+                     delay=delay,
+                     min_index=300001,
+                     max_index=None,
+                     step=step,
+                     batch_size=batch_size)
+
+# This is how many steps to draw from `val_gen`
+# in order to see the whole validation set:
+val_steps = (300000 - 200001 - lookback) // batch_size
+
+# This is how many steps to draw from `test_gen`
+# in order to see the whole test set:
+test_steps = (len(float_data) - 300001 - lookback) // batch_size
+
+
+print('prepare the climate data done')
+print('---------------------------------------------------------------------------------------------------------------')
+
+
+# 6.3.5 A first recurrent baseline
+
+# build network model: with GRU layer
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
+
+model = Sequential()
+model.add(layers.GRU(32, input_shape=(None, float_data.shape[-1])))
+model.add(layers.Dense(1))  # no activation def in last dense layer in regression problem model
+
+model.compile(optimizer=RMSprop(), loss='mae')  # loss
+history = model.fit_generator(train_gen,
+                              steps_per_epoch=500,
+                              epochs=20,
+                              validation_data=val_gen,
+                              validation_steps=val_steps)
+# loss: 4.1808 - val_loss: 3.9868
+
+
+# plot
+import matplotlib.pyplot as plt
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(loss))
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
