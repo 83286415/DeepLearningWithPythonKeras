200cb9badf35f0b8c32cb286c109dbd64d7d5405
83286415 <83286415@163.com>
2019-07-17 17:32:09 +0800

    6.4.4-combining-CNNs-and-RNNs-to-process-long-sequences.py added 5.2 py bug fixed Chapter 6 done

--- 5.2-using-convnets-with-small-datasets.py
+++ 5.2-using-convnets-with-small-datasets.py
@@ -125,8 +125,8 @@ from keras import models
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu',
                         input_shape=(150, 150, 3)))  # relu: make negative values into 0
-model.add(layers.MaxPooling2D((2, 2)))  # cut the out put map shape into half
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))  # increase tunnels from 32 into 64
+model.add(layers.MaxPooling2D((2, 2)))  # cut the out put map shape into half: (2, 2) 2 is pick one from 2
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))  # increase tunnels from 32 into 64; (3, 3) W gets 3*3=9 feature
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
